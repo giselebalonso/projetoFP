861a53eea984f42df0b08c825baebf1c640b1203
giselebalonso <gisele.barbosaalonso@yahoo.com.br>
2014-05-16 19:47:17 -0300

    Arrumando meu fluxoListar, que não quer listar =(

--- FluxoCaixa/views.py
+++ FluxoCaixa/views.py
@@ -11,8 +11,8 @@ def listarFluxos(request):
 def pesquisarFluxos(request):
     if request.method == 'POST':
         pessoaBuscar = request.POST.get('pessoaBuscar')
-        dataBuscaInicio = datetime.strptime(request.POST.get('dataBuscaInicio', ''), '%d/%m/%Y %H:%M:%S')
-        dataBuscaFim = datetime.strptime(request.POST.get('dataBuscaFinal', ''), '%d/%m/%Y %H:%M:%S')
+        dataBuscaInicio = datetime.strptime(request.POST.get('dataBuscaInicio', ''), '%d/%m/%Y')
+        dataBuscaFim = datetime.strptime(request.POST.get('dataBuscaFinal', ''), '%d/%m/%Y')
         
         nome = Pessoa.objects.filter(id=pessoaBuscar)
         pessoas = Pessoa.objects.all().order_by('nome')
@@ -32,4 +32,4 @@ def pesquisarFluxos(request):
         except:
             contas = [Conta(descricao='erro')]
 
-        return render(request, 'fluxos/listarFluxos.html', {'contas': contas, 'nome':nome, 'pessoas': pessoas, 'totalreceber':totalreceber,'totalpagar':totalpagar})
+        return render(request, 'fluxos/fluxoListar.html', {'contas': contas, 'nome':nome, 'pessoas': pessoas, 'totalreceber':totalreceber,'totalpagar':totalpagar})
